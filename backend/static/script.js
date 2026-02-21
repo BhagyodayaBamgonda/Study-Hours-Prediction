18295e541f38e5fb87c024b84659f39f386d36af
BhagyodayaBamgonda2 <gaminglegenda45@gmail.com>
2026-02-21 23:06:59 +0530

    completed all the work

--- backend/static/script.js
+++ backend/static/script.js
@@ -16,7 +16,7 @@ document.getElementById('predictionForm').addEventListener('submit', async (e) =
     };
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/predict', {
+        const response = await fetch("/predict", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
